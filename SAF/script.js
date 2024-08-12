6d2b9850ff146573eb9bf0b34be25bab2b943eb4
Giovanna D'Alcântara <142796669+alcangio@users.noreply.github.com>
2024-08-12 15:13:11 -0300

    Delete extra SAF directory

--- SAF/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('nav ul');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
